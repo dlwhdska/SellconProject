b66bf3c04f186b308b183c74a697e5f56e052efd
JongNam <ljn2474@naver.com>
2023-12-04 14:25:27 +0900

    로그인 기능 구현 완료

---
+++ src/main/java/com/sellcon/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.sellcon.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sellcon.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+	
+	Optional<Member> findById(String id);
+	
+	Optional<Member> findByEmail(String Email);
+}
